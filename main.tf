283be9246a2da5b2c4f490e534d90013f4fc34e8
lekshmidas <122253677+lekshmidas@users.noreply.github.com>
2025-11-23 18:03:00 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+   cloud {
+    organization = "Dhannya_Org"
+    workspaces {
+      name = "dhannya-tfc-demo"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "demo" {
+  bucket = "tfc-cli-demo-bucket-123-dhannya"
+}
